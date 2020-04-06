6cc5ce028812a0a559b4e40e26846e9535cef61b
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-07 00:00:47 +0200

    Add new action and improve code

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,11 +1,18 @@
-import { MESSAGE } from '../constants/action-types';
+import { MESSAGE, DATA_LOADING } from '../constants/action-types';
 //---------------------------------------------------------------------------
+export const fetchData = bool => ({
+    type: DATA_LOADING,
+    payload: bool
+})
+
 export function getData(url, type_data, key) {
     return function (dispatch) {
+        if(key === undefined)
+            dispatch(fetchData(true))
         return fetch(url)
             .then(response => response.json())
             .then(json => {
-                dispatch({ type: type_data, payload: json[MESSAGE], key: key });
+                dispatch({ type: type_data, payload: json[MESSAGE], key })
             }).catch(error => {
                 console.log(error);
             });
