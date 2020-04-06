43370403de97f657498bd80aced2947be663c2f7
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-07 00:02:14 +0200

    Add new constants and improve colors constant

--- src/js/constants/action-types.js
+++ src/js/constants/action-types.js
@@ -1,21 +1,23 @@
-export const ADD_ARTICLE = "ADD_ARTICLE";
+export const DATA_LOADING = "DATA_LOADING"
+export const DATA_LOADED = "DATA_LOADED"
+export const DATA_IMAGE_LOADED = "DATA_IMAGE_LOADED"
 //---------------------------------------------------------------------------
-export const DATA_LOADED = "DATA_LOADED";
-export const DATA_IMAGE_LOADED = "DATA_IMAGE_LOADED";
+export const URL = "https://dog.ceo/api/breeds/list/all"
+export const URL_IMAGE = "https://dog.ceo/api/breed/"
+export const IMAGES = "/images"
 //---------------------------------------------------------------------------
-export const URL = "https://dog.ceo/api/breeds/list/all";
-export const URL_IMAGE = "https://dog.ceo/api/breed/";
-export const IMAGES = "/images";
+export const MESSAGE = "message"
 //---------------------------------------------------------------------------
-export const MESSAGE = "message";
-//---------------------------------------------------------------------------
-export const RED = "red";
-export const PURPLE = "purple";
-export const BLUE = "blue";
-export const TEAL = "teal";
-export const GREEN = "green";
-export const ORANGE = "orange";
-export const YELLOW = "yellow";
-export const PINK = "pink";
-export const BROWN = "brown";
-export const GREY = "grey";
+export const COLORS = [
+    "red",
+    "purple",
+    "blue",
+    "teal",
+    "green",
+    "orange",
+    "yellow",
+    "pink",
+    "brown",
+    "grey"
+]
+
