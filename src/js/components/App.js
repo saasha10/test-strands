6a30775b2a55ccb2641b3a331db7dfc57487f9ed
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-07 00:00:11 +0200

    Refactor

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Post from './Post';
-//---------------------------------------------------------------------------
+import Post from '../containers/Post'
+
 const App = () => (
     <Post />
-);
-//---------------------------------------------------------------------------
+)
+
 export default App;
