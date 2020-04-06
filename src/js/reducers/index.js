7eb7202bba367f9857d746057a51772f327d200c
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-07 00:01:48 +0200

    Add the new action created and change switch by if

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,27 +1,33 @@
-import { DATA_LOADED } from '../constants/action-types';
-import { DATA_IMAGE_LOADED } from '../constants/action-types';
-//---------------------------------------------------------------------------
+import { DATA_LOADING, DATA_LOADED, DATA_IMAGE_LOADED } from '../constants/action-types'
+
 const initialState = {
+    loading: true,
     dogs: {},
     dogs_images: {}
-};
-//---------------------------------------------------------------------------
-function rootReducer(state = initialState, action) {
-    if (action.type === DATA_LOADED) {
-        return Object.assign({}, state, {
-            dogs: action.payload
-        });
-    }
-    else if(action.type === DATA_IMAGE_LOADED){
-        return Object.assign({}, state, {
-            dogs_images: {
-                ...state.dogs_images,
-                [action.key]: action.payload
-            }
-        });
-    }
+}
 
+function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        case DATA_LOADING:
+            return Object.assign({}, state, {
+                loading: action.payload
+            })
+        case DATA_LOADED:
+            return Object.assign({}, state, {
+                dogs: action.payload,
+                loading: false
+            });
+        case DATA_IMAGE_LOADED:
+            return Object.assign({}, state, {
+                dogs_images: {
+                    ...state.dogs_images,
+                    [action.key]: action.payload
+                }
+            });
+        default:
+            break;
+    }
     return state;
 }
-//---------------------------------------------------------------------------
-export default rootReducer;
+
+export default rootReducer
