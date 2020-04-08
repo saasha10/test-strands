20f926c9c8a1c41eda4ddc6a376a96b7d09a721d
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-08 12:42:20 +0200

    New index file in constants

---
+++ src/js/constants/index.js
@@ -0,0 +1,18 @@
+export const URL = "https://dog.ceo/api/breeds/list/all"
+export const URL_IMAGE = "https://dog.ceo/api/breed/"
+export const IMAGES = "/images"
+
+export const MESSAGE = "message"
+
+export const COLORS = [
+    "red",
+    "purple",
+    "blue",
+    "teal",
+    "green",
+    "orange",
+    "yellow",
+    "pink",
+    "brown",
+    "grey"
+]
