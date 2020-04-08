c346817b2c4277f2ae7b4552ac839b6741e4779c
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-08 12:05:14 +0200

    New action created

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,13 +1,19 @@
-import { MESSAGE, DATA_LOADING } from '../constants/action-types';
+import { MESSAGE, DATA_LOADING, DATA_IMAGE_LOADING } from '../constants/action-types';
 //---------------------------------------------------------------------------
 export const fetchData = bool => ({
     type: DATA_LOADING,
     payload: bool
 })
 
+export const fetchDataImages = bool => ({
+    type: DATA_IMAGE_LOADING,
+    payload: bool 
+})
+
 export function getData(url, type_data, key) {
     return function (dispatch) {
-        if(key === undefined)
+        const isGetDataDogsImages = key !== undefined
+        if(!isGetDataDogsImages)
             dispatch(fetchData(true))
         return fetch(url)
             .then(response => response.json())
