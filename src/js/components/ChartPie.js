6047ea63b3f829d8185eecb8e1eb791ef53255ad
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-08 12:34:24 +0200

    Refactor and change to functional component

--- src/js/components/ChartPie.js
+++ src/js/components/ChartPie.js
@@ -1,65 +1,58 @@
-import React, { Component } from "react"
-import _ from 'lodash'
+import orderBy from 'lodash/orderBy'
+import forEach from 'lodash/forEach'
 import Chart from 'chart.js'
 import { COLORS } from '../constants/action-types'
-//---------------------------------------------------------------------------
-export default class ChartPie extends Component {
-    //---------------------------------------------------------------------------
-    componentDidMount() {
-        console.log(this.props.dogsImages)
-        this.createChart(this.props.dogsImages);
-    }
-    //---------------------------------------------------------------------------
-    createChart = () => {
-        let array_labels = [];
-        let array_data = [];
-        // Descending order and new array created with top 10 elements
-        let top_10_images = _.orderBy(this.props.dogsImages, (e) => { return e.length }, ['desc']);
-        top_10_images = top_10_images.slice(0, 10);
-        // Get the keys and values to save in two array to pass to the Chart
-        _.forEach(this.props.dogsImages, (values1, key1) => {
-            _.forEach(top_10_images, (values2) => {
-                if (values1.length === values2.length && array_labels.indexOf(key1) === -1) {
-                    array_labels.push(key1);
-                    array_data.push(values1.length);
-                }
-            });
-        });
-        // Create a Pie Chart
-        var ctx = document.getElementById('myChart');
-        ctx.getContext('2d')
 
-        new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'pie',
-            // The data for our dataset
-            data: {
-                labels: array_labels,
-                datasets: [{
-                    label: 'Breeds',
-                    fill: true,
-                    backgroundColor: COLORS,
-                    borderColor: '#bdbdbd',
-                    data: array_data
-                }]
-            },
-            // Configuration options go here
-            options: {
-                title: {
-                    display: true,
-                    text: 'Breeds',
-                    position: 'top'
-                },
-                responsive: true,
-                responsiveAnimationDuration: 1500,
+function createChart ({dogsImages}) {
+    let labelsChart = []
+    let dataChart = []
+    // Descending order and new array created with top 10 elements
+    let top10images = orderBy(dogsImages, (e) => { return e.length }, ['desc'])
+    top10images = top10images.slice(0, 10)
+    // Get the keys and values to save in two array to pass to the Chart
+    forEach(dogsImages, (images, dog) => {
+        forEach(top10images, imagesTop10 => {
+            if (images.length === imagesTop10.length && labelsChart.indexOf(dog) === -1) {
+                labelsChart.push(dog);
+                dataChart.push(images.length)
             }
         });
-    }
-    //---------------------------------------------------------------------------
-    render() {
-        console.log("RENDER CHART")
-        return (
-            <canvas id="myChart"></canvas>
-        );
-    }
+    });
+    // Create a Pie Chart
+    var ctx = document.getElementById('myChart')
+    if(ctx !== null)
+        ctx.getContext('2d')
+
+    new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'pie',
+        // The data for our dataset
+        data: {
+            labels: labelsChart,
+            datasets: [{
+                label: 'Breeds',
+                fill: true,
+                backgroundColor: COLORS,
+                borderColor: '#bdbdbd',
+                data: dataChart
+            }]
+        },
+        // Configuration options go here
+        options: {
+            title: {
+                display: true,
+                text: 'Breeds',
+                position: 'top'
+            },
+            responsive: true,
+            responsiveAnimationDuration: 1500,
+        }
+    });
+}
+
+export default function ({dogsImages}) {
+    createChart({dogsImages})
+    return (
+        null
+    )
 }
