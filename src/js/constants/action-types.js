47caff6d92410d52a50e420956b563a1919816af
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-08 12:44:34 +0200

    Remove constants that aren't actions

--- src/js/constants/action-types.js
+++ src/js/constants/action-types.js
@@ -1,23 +1,5 @@
 export const DATA_LOADING = "DATA_LOADING"
 export const DATA_LOADED = "DATA_LOADED"
+export const DATA_IMAGE_LOADING = "DATA_IMAGE_LOADING"
 export const DATA_IMAGE_LOADED = "DATA_IMAGE_LOADED"
-//---------------------------------------------------------------------------
-export const URL = "https://dog.ceo/api/breeds/list/all"
-export const URL_IMAGE = "https://dog.ceo/api/breed/"
-export const IMAGES = "/images"
-//---------------------------------------------------------------------------
-export const MESSAGE = "message"
-//---------------------------------------------------------------------------
-export const COLORS = [
-    "red",
-    "purple",
-    "blue",
-    "teal",
-    "green",
-    "orange",
-    "yellow",
-    "pink",
-    "brown",
-    "grey"
-]
 
