207bd5cd16296e0d9e53db350c466e584dcc835d
saasha10 <50591864+saasha10@users.noreply.github.com>
2020-04-08 12:06:50 +0200

    Refactor in Images component

--- src/js/components/Images.js
+++ src/js/components/Images.js
@@ -1,32 +1,26 @@
 import React, { Component } from "react"
-import _ from 'lodash'
+import size from 'lodash/size'
 import Chart from './ChartPie'
 import { DATA_IMAGE_LOADED, URL_IMAGE, IMAGES } from '../constants/action-types'
-//---------------------------------------------------------------------------
-let count = 0;
-//---------------------------------------------------------------------------
-export default class Images extends Component {
-    //---------------------------------------------------------------------------
-    componentDidMount() {
-        let dogs = this.props.dogs;
-        _.forEach(dogs, (value, key) => {
-            count++;
-            this.props.getData(URL_IMAGE + key + IMAGES, DATA_IMAGE_LOADED, key);
-        });
-        
-    }
-    //---------------------------------------------------------------------------
-    render() {
-        let chart_load = null;
 
-        if(count === _.size(this.props.dogsImages) && _.size(this.props.dogsImages) !== 0){
-            chart_load = <Chart dogsImages={this.props.dogsImages}/>
-        }
+export default class Images extends Component {
+
+    componentDidMount() {
+        const dogs = this.props.dogs
+        
+        Object.keys(dogs).forEach( key => {
+            const urlImage = URL_IMAGE + key + IMAGES
+            this.props.getData(urlImage, DATA_IMAGE_LOADED, key)
+        })
+    }
+
+    render() {
+        const dogs = this.props.dogs
+        const dogsImages = this.props.dogsImages
+        const chartIsReady = size(dogs) === size(dogsImages)
 
         return (
-            <div className="container">
-                {chart_load}
-            </div>
-        );
+                chartIsReady && <Chart dogsImages={this.props.dogsImages}/>
+        )
     }
 }
